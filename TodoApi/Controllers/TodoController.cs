ca007363aa1dfb8b4ebd8b08ae15e7cb40f7e6c8
Mark Visschers <visschers.m@gmail.com>
2020-04-08 18:16:51 +0200

    Create method

--- TodoApi/Controllers/TodoController.cs
+++ TodoApi/Controllers/TodoController.cs
@@ -4,6 +4,8 @@ using MTech.RequestHandler;
 using MTech.TodoApp.TodoItem.Requests;
 using MTech.TodoApp.TodoItem.Results;
 using System.Threading.Tasks;
+using MTech.TodoApp.TodoItem.Commands;
+using MTech.TodoApp.ViewModel.TodoItem;
 
 namespace MTech.TodoApp.TodoApi
 {
@@ -17,13 +19,29 @@ namespace MTech.TodoApp.TodoApi
             _handler = handler;
         }
 
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(typeof(CreatedTodoItemView), StatusCodes.Status200OK)]
+        public async Task<IActionResult> Create([FromBody]ViewModel.TodoItem.CreateView toCreate)
+        {
+            var result = await _handler.HandleCommand<CreateTodoItemCommand, CreateTodoItemCommandResult>(
+                new CreateTodoItemCommand(toCreate));
+            
+            if(!result.Successfull)
+                return StatusCode(StatusCodes.Status500InternalServerError);
+
+            return Ok(result);
+        }
+
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(typeof(TodoItemListView[]), StatusCodes.Status200OK)]
         public async Task<IActionResult> Get()
         {
             var result = await _handler.HandleQuery<GetAllTodoItemsRequest, TodoItemListViewResult>(
                 new GetAllTodoItemsRequest());
 
-            if (!result.Succesfull)
+            if (!result.Successfull)
                 return StatusCode(StatusCodes.Status500InternalServerError);
 
             return Ok(result.Data);
@@ -36,7 +54,7 @@ namespace MTech.TodoApp.TodoApi
             var result = await _handler.HandleQuery<GetTodoItemByIdRequest, DetailedTodoItemViewResult>(
                 new GetTodoItemByIdRequest(id));
 
-            if(!result.Succesfull)
+            if(!result.Successfull)
                 return NotFound($"Could not find TodoItem with id: {id}");
             
             return Ok(result.Data);
