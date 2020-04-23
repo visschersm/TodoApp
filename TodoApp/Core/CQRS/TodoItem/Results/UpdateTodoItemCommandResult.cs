74405d92cc011c303bc4b6e8e4713aed436eea61
Mark Visschers <visschers.m@gmail.com>
2020-04-23 14:01:24 +0200

    Added Update Command Result

---
+++ TodoApp/Core/CQRS/TodoItem/Results/UpdateTodoItemCommandResult.cs
@@ -0,0 +1,10 @@
+﻿using MTech.Utilities.RequestHandler;
+
+namespace MTech.TodoApp.CQRS.Results
+{
+    public class UpdateTodoItemCommandResult : ICommandResult
+    {
+        public bool Successfull { get; set; }
+        public ViewModel.TodoItem.ListView Data { get; set; } = null!;
+    }
+}
