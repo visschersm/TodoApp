e4b278d38928a234e041670d460e0e27ce633a82
Mark Visschers <visschers.m@gmail.com>
2020-04-23 09:55:14 +0200

    Added EFCore friendly way to handle colors.

--- TodoApp/Core/Entities/TodoList.cs
+++ TodoApp/Core/Entities/TodoList.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Drawing;
 
 namespace MTech.TodoApp.Entities
@@ -9,6 +10,20 @@ namespace MTech.TodoApp.Entities
         public string Title { get; set; } = null!;
 
         public ICollection<TodoItem> TodoItems { get; set; } = new List<TodoItem>();
+
+        public int Argb
+        {
+            get
+            {
+                return LabelColor.ToArgb();
+            }
+            set
+            {
+                LabelColor = Color.FromArgb(value);
+            }
+        }
+
+        [NotMapped]
         public Color LabelColor { get; set; }
     }
 }
