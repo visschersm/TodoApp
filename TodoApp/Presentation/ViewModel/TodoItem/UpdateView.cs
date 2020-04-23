a99c123ae775eaf052b42a9e79f893c4c8451e1f
Mark Visschers <visschers.m@gmail.com>
2020-04-23 14:03:06 +0200

    Added update view

---
+++ TodoApp/Presentation/ViewModel/TodoItem/UpdateView.cs
@@ -0,0 +1,13 @@
+﻿using MTech.Utilities.ViewModel;
+using System;
+
+namespace MTech.TodoApp.ViewModel.TodoItem
+{
+    public class UpdateView : IViewOf<Entities.TodoItem>
+    {
+        public string Title { get; set; } = "";
+        public Priority Priority { get; set; }
+        public DateTime DueDate { get; set; }
+        public string? Note { get; set; }
+    }
+}
