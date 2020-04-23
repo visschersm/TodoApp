ce8074c45b9fc09868a2f57aba836630b829a118
Mark Visschers <visschers.m@gmail.com>
2020-04-23 14:01:05 +0200

    Added Query Result

---
+++ TodoApp/Core/CQRS/TodoItem/Results/GetTodoItemByIdQueryResult.cs
@@ -0,0 +1,11 @@
+﻿using MTech.TodoApp.ViewModel.TodoItem;
+using MTech.Utilities.RequestHandler;
+
+namespace MTech.TodoApp.CQRS.Results
+{
+    public class GetTodoItemByIdQueryResult : IQueryResult
+    {
+        public bool Successfull { get; set; }
+        public DetailedView Data { get; internal set; } = null!;
+    }
+}
