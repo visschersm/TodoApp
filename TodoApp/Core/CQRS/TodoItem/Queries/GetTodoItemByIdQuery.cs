bd776fccc80ba323bada1451256cac38e0c48215
Mark Visschers <visschers.m@gmail.com>
2020-04-23 14:00:55 +0200

    Added Query

---
+++ TodoApp/Core/CQRS/TodoItem/Queries/GetTodoItemByIdQuery.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MTech.TodoApp.CQRS.Results;
+using MTech.TodoApp.DataModel.Interfaces;
+using MTech.Utilities.RequestHandler;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MTech.TodoApp.CQRS.Queries
+{
+    public class GetTodoItemByIdQuery : IQueryRequest
+    {
+        private readonly int id;
+
+        public GetTodoItemByIdQuery(int id)
+        {
+            this.id = id;
+        }
+
+        public class GetTodoItemByIdQueryHandler : IQueryHandler<GetTodoItemByIdQuery, GetTodoItemByIdQueryResult>
+        {
+            private readonly ITodoContext _context;
+
+            public GetTodoItemByIdQueryHandler(ITodoContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<GetTodoItemByIdQueryResult> Handle(GetTodoItemByIdQuery request)
+            {
+                var result = await _context.TodoItems.Where(x => x.Id == request.id)
+                    .SingleOrDefaultAsync();
+
+                if (result == null)
+                    return new GetTodoItemByIdQueryResult { Successfull = false };
+
+                return new GetTodoItemByIdQueryResult
+                {
+                    Successfull = true,
+                    Data = new ViewModel.TodoItem.DetailedView
+                    {
+
+                    }
+                };
+            }
+        }
+    }
+}
