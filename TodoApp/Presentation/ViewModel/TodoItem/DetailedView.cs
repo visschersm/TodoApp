87789ee30029c3649b4115860ca09e99c48d5d3d
Mark Visschers <visschers.m@gmail.com>
2020-04-23 14:02:35 +0200

    Added properties

--- TodoApp/Presentation/ViewModel/TodoItem/DetailedView.cs
+++ TodoApp/Presentation/ViewModel/TodoItem/DetailedView.cs
@@ -1,8 +1,17 @@
 ﻿using MTech.Utilities.ViewModel;
+using System;
 
 namespace MTech.TodoApp.ViewModel.TodoItem
 {
     public class DetailedView : IViewOf<Entities.TodoItem>
     {
+        public int Id { get; set; }
+        public int ParentId { get; set; }
+        public string Title { get; set; } = "";
+        public Priority Priority { get; set; }
+        public Status Status { get; set; }
+        public DateTime DueDate { get; set; }
+        public string? Note { get; set; }
+
     }
 }
