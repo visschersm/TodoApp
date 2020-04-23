a1b63d82b2ad4b5a5910bbf79413833352eb7e9d
Mark Visschers <visschers.m@gmail.com>
2020-04-23 10:01:12 +0200

    Added Status and Note field

--- TodoApp/Core/Entities/TodoItem.cs
+++ TodoApp/Core/Entities/TodoItem.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MTech.TodoApp.Enumerations;
+using System;
 
 namespace MTech.TodoApp.Entities
 {
@@ -7,9 +8,10 @@ namespace MTech.TodoApp.Entities
         public int Id { get; set; }
         public string Title { get; set; } = null!;
         public Priority Priority { get; set; }
-        public bool IsDone { get; set; }
         public DateTime DueDate { get; set; }
         public int ParentId { get; set; }
         public DateTime CreatedDate { get; set; }
+        public string? Note { get; set; }
+        public Status Status { get; set; }
     }
 }
